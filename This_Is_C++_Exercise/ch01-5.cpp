4f03cccfbbd07f144dae5d01430760b679a31daf
ByeongGuk Park <ppkj58220@naver.com>
2022-07-08 14:28:12 +0900

    ch01-5

---
+++ This_Is_C++_Exercise/ch01-5.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	/*
+	기존의 참조 형식은 상수를 참조할 수 없다.
+	하지만 상수형 참조는 상수인 r-value를 참조 할 수 있으며 
+	이는 계산과정에서 사리질 결과인 임시 결과를 참조 하여 그 값을 변경하거나 수명을 늘릴 수 있다.
+	*/
+
+	return 0;
+}
