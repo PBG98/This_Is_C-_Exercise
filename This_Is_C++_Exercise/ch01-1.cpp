5971e81a5b9239c71d7d33c0efb55a7d4e2a362b
ByeongGuk Park <ppkj58220@naver.com>
2022-07-08 13:58:13 +0900

    ch01-1

--- This_Is_C++_Exercise/ch01-1.cpp
+++ This_Is_C++_Exercise/ch01-1.cpp
@@ -1,9 +1,20 @@
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	std::cout << "HelloWorld" << std::endl;
+	std::string szName;
+	int nAge;
 
+	std::cout << "What is your name?" << std::endl;
+	std::cin >> szName;
+
+	std::cout << "How old are you?" << std::endl;
+	std::cin >> nAge;
+
+	std::cout << "My name is " << szName << ", and I am " << nAge << std::endl;
+
+	
 	return 0;
 }
