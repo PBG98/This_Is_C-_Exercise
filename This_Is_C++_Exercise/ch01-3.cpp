692e1de7762c032428afa299f365d8ff4d831c0a
ByeongGuk Park <ppkj58220@naver.com>
2022-07-08 14:13:03 +0900

    ch01-3

---
+++ This_Is_C++_Exercise/ch01-3.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char* szData = new  char[12];
+
+	delete[] szData;
+
+	return 0;
+}
