2a5b3dd206512b0b29bd9c9a36f9f19fe6061e53
ByeongGuk Park <ppkj58220@naver.com>
2022-07-19 20:01:27 +0900

    ch03-4

---
+++ This_Is_C++_Exercise/ch03-4.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+class CRefTest
+{
+public:
+	CRefTest(int& rParam)
+		:m_nData(rParam)
+	{
+		//m_nData = rParam;
+	}
+
+	int GetData(void) { return m_nData; }
+
+
+private:
+	int& m_nData;
+
+};
